7bc87347eec3efc313ccdd2cfac625c81de12823
Amelie Oller <amelieoller@gmail.com>
2019-10-05 11:11:36 -0700

    Update button component colors and styles

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -5,45 +5,67 @@ import styled, { css } from 'styled-components';
 const propsCSS = {
   outline: css`
     background-color: white;
-    color: rgb(61, 100, 255);
-    border: 1px solid rgb(61, 100, 255);
+    color: #70C1B3;
+    border: 2px solid #70C1B3;
+
     &:hover {
-      background-color: ${props => !props.disabled && 'rgb(43, 75, 191)'};
-    }
-    &:focus {
-      background-color: ${props => !props.disabled && 'rgb(230, 230, 230)'};
+      background-color: ${props => !props.disabled && '#70C1B3'};
+      color: ${props => !props.disabled && 'white'};
     }
   `,
+
   disabled: css`
-    background-color: ${props => (props.outline ? 'white' : 'rgb(230, 230, 230)')};
+    background-color: ${props =>
+      props.outline ? 'white' : 'rgb(230, 230, 230)'};
     border-color: rgb(230, 230, 230);
-    color: ${props => (props.outline ? 'rgb(142, 142, 142)' : 'rgb(182, 182, 182)')};
+    color: ${props =>
+      props.outline ? 'rgb(142, 142, 142)' : 'rgb(182, 182, 182)'};
     cursor: not-allowed;
+
+    &:hover {
+      box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.14),
+        0 3px 1px -2px rgba(0, 0, 0, 0.2), 0 1px 5px 0 rgba(0, 0, 0, 0.12);
+    }
   `
 };
 
 const StyledButton = styled.button`
-  width: 200px;
-
-  background: rgb(61, 100, 255);
+  background: #70C1B3;
   display: inline-block;
   justify-content: center;
   align-items: center;
-  padding: 10px 0;
   outline: none;
   cursor: pointer;
 
   border-radius: 3px;
   border: ${props =>
-    props.outline ? '1px solid rgb(43, 75, 191)' : '1px solid transparent'};
+    props.outline ? '2px solid rgb(43, 75, 191)' : '2px solid transparent'};
 
   font-size: 16px;
   letter-spacing: 0.5px;
   color: white;
 
-  &:hover,
-  &:focus {
-    border: 1px solid rgb(43, 75, 191);
+  position: relative;
+  margin: 3em 0;
+  padding: 0.75em 3em;
+  box-shadow: 0.28s ease;
+  transition: background-color 0.28s ease, color 0.28s ease,
+    box-shadow 0.28s ease;
+  overflow: hidden;
+  box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 3px 1px -2px rgba(0, 0, 0, 0.2),
+    0 1px 5px 0 rgba(0, 0, 0, 0.12);
+
+  &:hover {
+    box-shadow: 0 6px 10px 0 rgba(0, 0, 0, 0.14),
+      0 1px 18px 0 rgba(0, 0, 0, 0.12), 0 3px 5px -1px rgba(0, 0, 0, 0.2);
+  }
+
+  &:active::before,
+  &:focus::before {
+    transition: transform 1.12s ease, opacity 0.28s ease 0.364s;
+    transition: transform 1.12s ease, opacity 0.28s ease 0.364s;
+    transform: translate(-50%, -50%) scale(1);
+    opacity: 0;
   }
 
   ${props => props.outline && propsCSS.outline};
