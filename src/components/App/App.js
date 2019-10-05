649c32b00168654328b76bc66d0c8ebbd545487b
Amelie Oller <amelieoller@gmail.com>
2019-10-04 19:55:08 -0700

    Update file/folder structure

---
+++ src/components/App/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+import Button from '../Button';
+
+const StyledApp = styled.div`
+  background: #eaeaea;
+  padding: 2em;
+`;
+
+function App() {
+  return (
+    <StyledApp>
+    </StyledApp>
+  );
+}
+
+export default App;
