fe209439a5bed394cb2c9f32f4ff5a722fd6ccbc
Amelie Oller <amelieoller@gmail.com>
2019-10-04 21:10:50 -0700

    Add plus svg icon

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,34 @@
 import React from 'react';
 import styled from 'styled-components';
+import { ReactComponent as Plus } from '../../assets/icons/plus.svg';
 
-const StyledHome = styled.div``;
+const StyledHome = styled.div`
+  h1 {
+    text-align: center;
+  }
+`;
+
+const StyledPlus = styled.div`
+  width: 17px;
+  display: inline-block;
+  padding-left: 4px;
+
+  path {
+    fill: salmon;
+  }
+`;
 
 function Home() {
-  return <StyledHome>Home</StyledHome>;
+  return (
+    <StyledHome>
+      <h1>
+        My Projects
+        <StyledPlus>
+          <Plus />
+        </StyledPlus>
+      </h1>
+    </StyledHome>
+  );
 }
 
 export default Home;
