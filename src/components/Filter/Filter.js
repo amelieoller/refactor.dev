a12a7913004090bc95f50496403737d040838c59
Amelie Oller <amelieoller@gmail.com>
2019-11-19 08:11:58 -0800

    Create badge component and remove unused sort

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -2,30 +2,14 @@ import React, { useContext } from 'react';
 import styled from 'styled-components';
 import { TagsContext } from '../../providers/TagsProvider';
 import { ProjectsContext } from '../../providers/ProjectsProvider';
+import Badge from '../Badge/Badge';
 
 const StyledFilter = styled.div`
   margin-bottom: 1.4rem;
 
-  .badge {
-    display: inline-block;
-    font-size: 1.4rem;
-    border-radius: 0.3rem;
-    padding: 0.2rem 1rem;
-    border: 1px solid ${props => props.theme.primary};
-    color: ${props => props.theme.primary};
+  & > span {
     margin-right: 1rem;
     margin-bottom: 1rem;
-    cursor: pointer;
-
-    &.selected {
-      background: ${props => props.theme.primary};
-      color: white;
-    }
-
-    &:hover {
-      background: ${props => props.theme.primaryDark};
-      color: white;
-    }
   }
 
   @media (max-width: 750px) {
@@ -40,14 +24,12 @@ const Filter = () => {
   return (
     <StyledFilter>
       {tags.map(tag => (
-        <span
+        <Badge
           key={tag.id}
-          className="badge"
-          className={filter.tags.includes(tag.name) ? 'badge selected' : 'badge'}
-          onClick={() => updateFilter('tags', tag.name)}
-        >
-          {tag.name}
-        </span>
+          badgeText={tag.name}
+          handleSelect={() => updateFilter('tags', tag.name)}
+          isSelected={filter.tags.includes(tag.name)}
+        />
       ))}
     </StyledFilter>
   );
