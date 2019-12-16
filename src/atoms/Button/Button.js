63880bb2fdcb130b1b54f8240a90b96593f281d6
Amelie Oller <amelieoller@gmail.com>
2019-12-15 19:49:46 -0800

    Move Button component

---
+++ src/atoms/Button/Button.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const StyledButton = styled.button`
+  font-size: 1.4rem;
+  border-radius: 0.3rem;
+  padding: 0.8rem 1.8rem;
+  border: 1px solid
+    ${props => (props.full ? props.theme.primaryDark : props.theme.primary)};
+  color: ${props => (props.full ? 'white' : props.theme.primary)};
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  background: ${props => (props.full ? props.theme.primary : 'transparent')};
+
+  svg {
+    margin-right: 0.5rem;
+  }
+
+  &.selected {
+    background: ${({ theme }) => theme.primary};
+    color: white;
+  }
+
+  &:hover {
+    background: ${props => (props.full ? props.theme.primaryDark : props.theme.primary)};
+    color: white;
+  }
+`;
+
+const Button = ({ full, children, ...props }) => (
+  <StyledButton full={full} {...props}>
+    {children}
+  </StyledButton>
+);
+
+Button.propTypes = {
+  children: PropTypes.node.isRequired
+};
+
+export default Button;
