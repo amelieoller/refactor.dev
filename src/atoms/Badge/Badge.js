db346f98b02801eb11e1858da8d99f87c9033fa7
Amelie Oller <amelieoller@gmail.com>
2019-12-15 19:48:37 -0800

    Create Badge component

---
+++ src/atoms/Badge/Badge.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const StyledBadge = styled.span`
+  display: inline-block;
+  font-size: 1.4rem;
+  border-radius: 0.3rem;
+  padding: 0.2rem 1rem;
+  border: 1px solid
+    ${props => (props.isSelected ? props.theme.primaryDark : props.theme.lighterGrey)};
+  color: ${props => (props.isSelected ? 'white' : props.theme.darkerGrey)};
+  cursor: pointer;
+  background: ${props =>
+    props.isSelected ? props.theme.primary : props.theme.lightestGrey};
+
+  &:hover {
+    background: ${({ theme }) => theme.primary};
+    border: 1px solid ${({ theme }) => theme.primaryDark};
+    color: white;
+  }
+`;
+
+const Badge = ({ badgeText, handleSelect, isSelected, className }) => {
+  return (
+    <StyledBadge isSelected={isSelected} onClick={handleSelect} className={className}>
+      {badgeText}
+    </StyledBadge>
+  );
+};
+
+Badge.defaultProps = {
+  isSelected: false
+};
+
+Badge.propTypes = {
+  badgeText: PropTypes.string.isRequired,
+  handleSelect: PropTypes.func.isRequired,
+  isSelected: PropTypes.bool.isRequired,
+  className: PropTypes.string
+};
+
+export default Badge;
