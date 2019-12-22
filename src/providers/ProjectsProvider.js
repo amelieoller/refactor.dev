c5744d3df4f513b2ba75a78bce6eb4843e0362c0
Amelie Oller <amelieoller@gmail.com>
2019-12-21 17:03:37 -0800

    Make projects provider functional and user based

--- src/providers/ProjectsProvider.js
+++ src/providers/ProjectsProvider.js
@@ -1,27 +1,66 @@
-import React, { Component, createContext } from "react";
-import { firestore } from "../firebase";
-import { collectIdsAndData } from "../utils/utilities";
+import React, { createContext, useState, useEffect } from 'react';
+import { withRouter } from 'react-router-dom';
+
+import { auth, createUserProfileDocument, firestore } from '../firebase';
+import { collectIdsAndData } from '../utils/utilities';
 
 export const ProjectsContext = createContext();
 
-class ProjectsProvider extends Component {
-  state = { projects: [], filter: { tags: [] } };
+const ProjectsProvider = ({ children }) => {
+  const [projects, setProjects] = useState(null);
+  const [filter, setFilter] = useState({ tags: [] });
+  const [sortBy, setSortBy] = useState('starred');
+  const [user, setUser] = useState(null);
+  const [tags, setTags] = useState(null);
 
-  unsubscribe = null;
+  useEffect(() => {
+    let unsubscribeFromAuth = null;
 
-  componentDidMount = () => {
-    this.unsubscribe = firestore.collection("projects").orderBy("updated", "desc").onSnapshot(snapshot => {
-      const projects = snapshot.docs.map(collectIdsAndData);
-      this.setState({ projects });
+    unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      const user = await createUserProfileDocument(userAuth);
+
+      setUser(user);
     });
-  };
 
-  componentWillUnmount = () => {
-    this.unsubscribe();
-  };
+    return () => {
+      unsubscribeFromAuth && unsubscribeFromAuth();
+    };
+  }, []);
 
-  updateFilter = (type, selectedItem) => {
-    const { filter } = this.state;
+  useEffect(() => {
+    let unsubscribeFromProjects = null;
+    let unsubscribeFromTags = null;
+
+    if (!!user) {
+      // Get projects for this user
+      unsubscribeFromProjects = firestore
+        .collection('users')
+        .doc(user.uid)
+        .collection('projects')
+        .onSnapshot(snapshot => {
+          const projects = snapshot.docs.map(collectIdsAndData);
+          setProjects(projects);
+        });
+
+      // Get tags for this user
+      unsubscribeFromTags = firestore
+        .collection('users')
+        .doc(user.uid)
+        .collection('tags')
+        .orderBy('name', 'asc')
+        .onSnapshot(snapshot => {
+          const tags = snapshot.docs.map(collectIdsAndData);
+          setTags(tags);
+        });
+    }
+
+    return () => {
+      unsubscribeFromProjects && unsubscribeFromProjects();
+      unsubscribeFromTags && unsubscribeFromTags();
+    };
+  }, [user]);
+
+  const updateFilter = (type, selectedItem) => {
     let newItems;
 
     if (filter[type].includes(selectedItem)) {
@@ -33,43 +72,72 @@ class ProjectsProvider extends Component {
       newItems = [...filter[type], selectedItem];
     }
 
-    this.setState({
-      filter: { [type]: newItems }
-    });
+    setFilter({ [type]: newItems });
   };
 
-  static applyFilter(projects, filter) {
-    let filteredProjects;
+  const updateSort = toSortBy => {
+    setSortBy(toSortBy);
+  };
 
+  const applyFilterAndSort = (projects, filter, sortBy) => {
+    let filteredAndSortedProjects = projects;
+
+    // Filter
     if (filter.tags.length === 0) {
-      filteredProjects = projects;
+      filteredAndSortedProjects = projects;
     } else {
-      filteredProjects = projects.filter(
-        r => filter.tags.every(tag => -1 !== r.tags.indexOf(tag))
+      filteredAndSortedProjects = projects.filter(r =>
+        filter.tags.every(tag => -1 !== r.tags.indexOf(tag))
       );
     }
 
-    return filteredProjects;
-  }
+    // Sort
+    if (sortBy === 'name') {
+      // sort by name
+      filteredAndSortedProjects.sort((a, b) => a.title.localeCompare(b.title));
+    } else if (sortBy === 'updated') {
+      // sort by date
+      filteredAndSortedProjects.sort((a, b) => b.updated.seconds - a.updated.seconds);
+    } else if (sortBy === 'starred') {
+      // put starred first, then sort by last updated
+      filteredAndSortedProjects.sort((a, b) => {
+        if (a.starred) {
+          return -1;
+        } else if (b.starred) {
+          return 1;
+        } else {
+          return b.updated.seconds - a.updated.seconds;
+        }
+      });
+    }
 
-  render() {
-    const { projects, filter } = this.state;
-    const { children } = this.props;
+    // Filter
+    if (filter.tags.length !== 0) {
+      filteredAndSortedProjects = projects.filter(r =>
+        filter.tags.every(tag => -1 !== r.tags.indexOf(tag))
+      );
+    }
 
-    const filteredProjects = ProjectsProvider.applyFilter(projects, filter);
+    return filteredAndSortedProjects;
+  };
 
-    return (
-      <ProjectsContext.Provider
-        value={{
-          projects: filteredProjects,
-          updateFilter: this.updateFilter,
-          filter
-        }}
-      >
-        {children}
-      </ProjectsContext.Provider>
-    );
-  }
-}
+  const filteredProjects = projects && applyFilterAndSort(projects, filter, sortBy);
 
-export default ProjectsProvider;
+  return (
+    <ProjectsContext.Provider
+      value={{
+        projects: filteredProjects,
+        updateFilter: updateFilter,
+        updateSort: updateSort,
+        selectedSort: sortBy,
+        user,
+        filter,
+        tags
+      }}
+    >
+      {children}
+    </ProjectsContext.Provider>
+  );
+};
+
+export default withRouter(ProjectsProvider);
