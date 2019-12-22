e53c5bb7189f77f0fd062b4706a25939c171ce64
Amelie Oller <amelieoller@gmail.com>
2019-12-21 16:27:33 -0800

    Update theme colors

--- src/theme.js
+++ src/theme.js
@@ -2,9 +2,27 @@ const theme = {
   mainBackground: 'white',
   primary: '#776eff',
   primaryDark: '#675dfc',
-  primaryLight: '#CAC6FF',
+  primaryLight: '#a5a0fb',
   primaryBackground: '#ff9a9a',
-  text: '#50514f'
+  text: '#50514f',
+  textDark: '#323C47',
+  darkerGrey: '#989898',
+  grey: '#BAC5CD',
+  borderColor: '#D0D7DD',
+  borderColorLight: '#ebebeb',
+  lightGrey: '#E3E8EB',
+  lighterGrey: '#E9EFF4',
+  lightestGrey: '#F4F7F9',
+  transparentWhite: '#ffffffba',
+  border: '1px solid #D0D7DD',
+
+  sizes: {
+    borderRadius: '3px'
+  },
+
+  transitions: {
+    ease: '0.2s ease'
+  }
 };
 
 export default theme;
