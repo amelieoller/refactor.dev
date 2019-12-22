b9d8e425cf3e7dd0119e88a2b9d0234fb43c61cb
Amelie Oller <amelieoller@gmail.com>
2019-12-21 16:58:18 -0800

    Update badge styling, add counter

--- src/atoms/Badge/Badge.js
+++ src/atoms/Badge/Badge.js
@@ -8,14 +8,12 @@ const StyledBadge = styled.span`
   border-radius: 0.3rem;
   padding: 0.2rem 1rem;
   cursor: pointer;
+  position: relative;
   border: 1px solid
     ${props => (props.isSelected ? props.theme.primaryDark : props.theme.lighterGrey)};
   color: ${props => (props.isSelected ? 'white' : props.theme.darkerGrey)};
   background: ${props =>
     props.isSelected ? props.theme.primary : props.theme.lightestGrey};
-  /* border: 1px solid ${props => props.theme.primaryDark};
-  color: ${props => props.theme.darkerGrey};
-  background: ${props => props.theme.lightestGrey}; */
 
   &:hover {
     background: ${({ theme }) => theme.primary};
@@ -24,10 +22,28 @@ const StyledBadge = styled.span`
   }
 `;
 
-const Badge = ({ badgeText, handleClick, isSelected, className }) => {
+const Counter = styled.span`
+  position: absolute;
+  top: -9px;
+  right: -9px;
+  font-size: 0.7em;
+  border-radius: 50%;
+  background: white;
+  border: 1px solid ${({ theme }) => theme.primary};
+  color: ${({ theme }) => theme.text};
+  display: inline-block;
+  border-radius: 50%;
+  text-align: center;
+  width: 18px;
+  line-height: 16px;
+  font-weight: 600;
+`;
+
+const Badge = ({ badgeText, handleClick, isSelected, className, count }) => {
   return (
     <StyledBadge isSelected={isSelected} onClick={handleClick} className={className}>
       {badgeText}
+      {count && <Counter className="badge">{count}</Counter>}
     </StyledBadge>
   );
 };
